07374c1e0e1cbfb359abfd93a9d115273616c682
Gerben Neven <gerbyzation@me.com>
2015-05-08 12:40:18 +0100

    fixed accordion issue

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static('app'));
 
+app.get('/export', function (req, res) {
+  // concat all files together needed for embedding
+  
+});
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + req.path));
 });
