df25594e14eab32d75014798c779c4d732a0d308
Gerben Neven <gerbyzation@me.com>
2015-05-08 19:09:08 +0100

    cleaning up, adding export

--- app/scripts/directives/engine.js
+++ app/scripts/directives/engine.js
@@ -3,6 +3,6 @@ angular.module('engineDir', []).directive('enginedir', [ function () {
   return {
     restrict: 'E',
     replace: true,
-    template: '<canvas id="canvas" width="1240" height="720">Your browser doesn\'t seem to support HTML5 :(</canvas>'
+    template: '<canvas id="canvas" width="1280" height="720">Your browser doesn\'t seem to support HTML5 :(</canvas>'
   };
 }]);
